4fd492900842ccfaf349e617aa562517d385f252
Malnati <ricardomalnati@gmail.com>
2025-04-02 00:51:31 -0300

    Adiciona novas funções ao módulo version_analyzer no arquivo __init__.py, melhorando a funcionalidade do código e a organização das importações.

--- src/core/utils/__init__.py
+++ src/core/utils/__init__.py
@@ -15,4 +15,10 @@ from .logger import (
 )
 from .data_masking import mask_sensitive_data
 from .token_validator import TokenValidator
-from .version_analyzer import analyze_version 
+from .version_analyzer import (
+    analyze_commit_message,
+    get_current_version,
+    increment_version,
+    update_version_files,
+    smart_bump
+) 
